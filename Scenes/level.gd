a7a2155442dbd01fdd920cac05e4e23c8629923f
unknown <rhiki.080bct033@tcioe.edu.np>
2024-12-06 18:08:21 +0545

    Added controller controls and vibration

--- Scenes/level.gd
+++ Scenes/level.gd
@@ -2,8 +2,12 @@ extends Node2D
 #load scene
 var enemy_scene: PackedScene = load("res://Scenes/meteor.tscn")
 var laser_scene: PackedScene = load("res://Scenes/laser.tscn")
-var health: int = 4
+var health: int = 5
+
+
 func _on_enemy_timer_timeout():
+	if get_tree().paused:
+		return
 	#create instance
 	var enemy = enemy_scene.instantiate()
 	
@@ -14,6 +18,7 @@ func _on_enemy_timer_timeout():
 
 func _on_enemy_collision():
 	health-=1
+	Input.start_joy_vibration(0,0.5,1,0.3)
 	get_tree().call_group("UI","_sethealth",health)
 	if health<=0:
 		get_tree().change_scene_to_file("res://Scenes/game_over.tscn")
@@ -22,12 +27,10 @@ func _on_enemy_collision():
 
 func _ready():
 	get_tree().call_group("UI","_sethealth",health)
-	
-	
+	get_tree().paused = false
 	 
 func _on_player_laser(pos):
 	var laser = laser_scene.instantiate()
 	$Laser.add_child(laser)
-	laser.position = pos 
-	
+	laser.position = pos
 	
